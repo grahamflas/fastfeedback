98986c908bcd96d8dc1f5f40fc2059fcb970a78a
Graham Flaspoehler <grahamflas@gmail.com>
2024-04-30 08:52:22 -0400

    Hello, world!

--- app/page.tsx
+++ app/page.tsx
@@ -40,6 +40,8 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+        <h2 className="mt-3 mb-3 text-2xl font-semibold">FastFeedback</h2>
+
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
